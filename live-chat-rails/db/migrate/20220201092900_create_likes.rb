cf226886bcb5467fc2d90ffc6cb6667b27b9e39b
koheihida <koheihida04@gmail.com>
2022-02-01 18:31:17 +0900

    マイグレーションの実行

--- live-chat-rails/db/migrate/20220201092900_create_likes.rb
+++ live-chat-rails/db/migrate/20220201092900_create_likes.rb
@@ -1,7 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[6.0]
   def change
     create_table :likes do |t|
-
+      t.references :user
+      t.references :message
       t.timestamps
     end
   end
