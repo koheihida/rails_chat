d4d36112bc5d259b883802b2e11b32e9b6629b75
koheihida <koheihida04@gmail.com>
2022-02-02 19:09:14 +0900

    Action Cableの実装

--- live-chat-rails/app/channels/room_channel.rb
+++ live-chat-rails/app/channels/room_channel.rb
@@ -1,9 +1,17 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "some_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def receive(data)
+    user = User.find_by(email:data['email'])
+
+    if mmessage = Message.create(content:data['message'],user_id:user.id)
+      ActionCable.server.broadcast'room_channel',{message:data['message'],name:user.name,created_at:message.created_at}
+    end
   end
 end
