a21066271e722b166a43cdc57b86eb5c8045f55c
koheihida <koheihida04@gmail.com>
2022-02-03 18:40:16 +0900

    Navbar.vueの表示実装

--- live-chat-vuejs/src/views/Chatroom.vue
+++ live-chat-vuejs/src/views/Chatroom.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>チャットルームです</div>
+  <div class="container">
+    <Navbar />
+  </div>
 </template>
 
 <script>
-export default {
-}
+  import Navbar from '../components/Navbar'
+  export default {
+    components:{ Navbar },
+  }
+
 </script>
 
 <style scoped>
